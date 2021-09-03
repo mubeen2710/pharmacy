870b6c328f8a7cf1cfcbcd81e6c14f43c67e340e
mubeen2710 <mubeen27pkd@gmail.com>
2021-09-03 12:52:11 +0530

    third

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,12 @@
 const {Pool, Client } = require('pg');
 
- let connection =new Pool({
+ let connection =new Client({
  	  user: 'sqyjgmixgscolg',
  	  host: 'ec2-3-230-61-252.compute-1.amazonaws.com',
  	  database: 'dd91abvt6of0n9',
  	  password: 'eae4678345e128a41dc2772f253ed4f8ec1621d4d1b81da3c4ec527ed6484367',
  	  port: 5432,
-	   timeout=100,
+	  
 	   ssl: {
 		rejectUnauthorized: false
 	  }
